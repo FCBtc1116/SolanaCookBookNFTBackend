b7be82f86c31e9d119fada840790731856776bdb
ichirodaichi <ichirodaichi0829@gmail.com>
2022-05-05 23:51:23 +0900

    Test

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import key from "./assets/key.json";
 import { actions, NodeWallet, Wallet } from '@metaplex/js';
 import { Metadata } from '@metaplex-foundation/mpl-token-metadata';
 import { Keypair, SystemProgram, Transaction, Connection, PublicKey, sendAndConfirmTransaction, clusterApiUrl } from '@solana/web3.js';
+import { ASSOCIATED_TOKEN_PROGRAM_ID, Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
 require('dotenv').config()
 
@@ -17,7 +18,6 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/mintNFTData", async function (req, res) {
-  let returnArray = [];
   let videoUrl;
   let metadata = undefined;  
   const bundlr = new Bundlr("https://devnet.bundlr.network", "solana", key, { providerUrl: "https://api.devnet.solana.com" });
@@ -32,12 +32,12 @@ app.get("/mintNFTData", async function (req, res) {
     await bundlr.fund(100_000_000);
     const tags = [{name: "Content-Type", value: "image/png"}];
 
-    const transaction = await bundlr.createTransaction(fs.readFileSync(`./assets/metadata/${i + 1}.png`), {tags : tags});
+    const bundltransaction = await bundlr.createTransaction(fs.readFileSync(`./assets/metadata/${i + 1}.png`), {tags : tags});
     
-    await transaction.sign();
-    await transaction.upload();
+    await bundltransaction.sign();
+    await bundltransaction.upload();
 
-    const { id } = transaction;
+    const { id } = bundltransaction;
     videoUrl = id ? `https://arweave.net/${id}` : undefined;
     console.log(videoUrl);
 
@@ -75,20 +75,70 @@ app.get("/mintNFTData", async function (req, res) {
 
     console.log(videoUrl);
 
-    const connection = new Connection(
-      clusterApiUrl('devnet'),
-      'confirmed',
-    );
+    const connection = new Connection("https://api.devnet.solana.com");
   
+    //Step1. Mint NFT
     const mintNFTResponse = await actions.mintNFT({
       connection,
       wallet: new NodeWallet(keypair),
       uri: videoUrl!,
       maxSupply: 1
     });
-    await new Promise(f => setTimeout(f, 10000));
+    console.log(`${i} NFTs are minted ${mintNFTResponse.mint.toBase58()}`);
+
+    // step 2. create associated account to user
+    let ata = await Token.getAssociatedTokenAddress(
+      ASSOCIATED_TOKEN_PROGRAM_ID, // always ASSOCIATED_TOKEN_PROGRAM_ID
+      TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
+      mintNFTResponse.mint, // mint
+      keypair.publicKey // owner
+    );
+    console.log(`ATA: ${ata.toBase58()}`);
+    console.log(keypair.publicKey.toBase58());
+    let tx = new Transaction().add(
+      Token.createAssociatedTokenAccountInstruction(
+        ASSOCIATED_TOKEN_PROGRAM_ID, // always ASSOCIATED_TOKEN_PROGRAM_ID
+        TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
+        mintNFTResponse.mint, // mint
+        ata, // ata
+        keypair.publicKey, // owner of token account
+        keypair.publicKey // fee payer
+      )
+    );
+    await (new Promise(resolve => setTimeout(resolve, 20000)));
+    console.log(tx.instructions);
+    let rlt = await connection.sendTransaction(tx, [keypair]);
+    console.log(rlt);
+
+    // step 3. send NFT to user
+    // Add token transfer instructions to transaction
+    let fromTokenAccount = await Token.getAssociatedTokenAddress(
+      ASSOCIATED_TOKEN_PROGRAM_ID, // always ASSOCIATED_TOKEN_PROGRAM_ID
+      TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
+      mintNFTResponse.mint, // mint
+      keypair.publicKey // owner
+    );
+    let toTokenAccount = ata;
+    await (new Promise(resolve => setTimeout(resolve, 20000)));
+    const transaction = new Transaction().add(
+      Token.createTransferInstruction(
+        TOKEN_PROGRAM_ID,
+        fromTokenAccount,
+        toTokenAccount,
+        keypair.publicKey,
+        [],
+        1,
+      ),
+    );
+
+    // Sign transaction, broadcast, and confirm
+    let finalResult = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [keypair]
+    );
+    console.log("finalResult", finalResult);
   
-    console.log(mintNFTResponse.mint.toBase58());
   }
   res.json(true);
 });
